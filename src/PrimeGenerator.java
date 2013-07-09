94ee585c08ccf9fe79dd1e7b68f82ddab81a4d44
tzhou13 <tzhou13@berkeley.edu>
2013-07-09 01:38:52 -0700

    Added range to be checked
    Also removed basic multiples

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -1,10 +1,15 @@
 import java.util.*;
+import java.math.*;
 
 public class PrimeGenerator implements Iterator<Integer> {
 	
 	public static void main (String [ ] args) {
 		// Make sure there is exactly one argument, an integer.
 		// Store the argument, converted to an int, in n.
+
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		sc.close();
 		PrimeGenerator primeGen = new PrimeGenerator (n);
 		int mostRecentPrime = 0;
 		while (primeGen.hasNext ( )) {
@@ -13,9 +18,23 @@ public class PrimeGenerator implements Iterator<Integer> {
 		System.out.println("The nth prime is: " + mostRecentPrime + " where n was: " + n);
 	}
 	
-	public PrimeGenerator (int n) {
+	public PrimeGenerator (int n) throws IllegalArgumentException{
 		// Make sure n is positive; throw IllegalArgumentException if not.
 		// Initialize the iterator of the first n prime numbers.
+		if (n<=0) {
+			throw new IllegalArgumentException("n is not positive");
+		}
+		ArrayList <Integer> values= new ArrayList<Integer>();
+		int start = (int) (n*Math.log(n)+n*(Math.log(Math.log(n))-1));
+		int end = (int) (n*Math.log(n)+n*(Math.log(Math.log(n))));
+		for (int i = start; i<=end;i++) {
+			if (i%2 != 0 //get rid of multiples of 2, 3, 5
+					&& i%3 != 0 
+					&& i%5 != 0) {
+				values.add(i);
+			}
+		}
+
 	}
 	
 	// Return true if there are more primes to return.
